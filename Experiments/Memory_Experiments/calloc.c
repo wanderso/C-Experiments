aacf6b3fd058adcbf648125a9976fc81a48c107a
William Anderson <williamharrisanderson@gmail.com>
2016-12-28 20:02:09 -0800

    Calloc stuff. Starting work on event loop now.

--- Experiments/Memory_Experiments/calloc.c
+++ Experiments/Memory_Experiments/calloc.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LIFE_WORTH_LIVING 0
+#define DEFAULT_ARRAY_LENGTH 10
+#define ARRAY_LENGTH_STEP 5
+
 typedef struct array {
     int * array;
     int arrayLen;
     int curr_maxLen;
 } array_t;
 
+array_t newArray(void) {
+    array_t createArray;
+    createArray.array = (int*) calloc (DEFAULT_ARRAY_LENGTH,sizeof(int));
+    createArray.arrayLen = 0;
+    createArray.curr_maxLen = DEFAULT_ARRAY_LENGTH;
+    return createArray;
+}
+
+void destroyArray(array_t target)
+{
+    free (target.array);
+}
+
 int main() {
-    
+    array_t arrv;
+    arrv = newArray();
+    printf("%p,%d,%d\n", arrv.array, arrv.arrayLen, arrv.curr_maxLen);
+    destroyArray(arrv);
     return 0;
 }
