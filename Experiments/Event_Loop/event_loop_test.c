f46edd96828c478fd6b14500e391883dd07390ad
William Anderson <williamharrisanderson@gmail.com>
2016-12-29 10:12:04 -0800

    More event loop stuff

--- Experiments/Event_Loop/event_loop_test.c
+++ Experiments/Event_Loop/event_loop_test.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-   FILE * fp;
-   
-   fp = fopen ("file.txt", "w+");
-   
-   fclose(fp);
-   
+   FILE *fr;
+   int n;
+   long elapsed_seconds;
+   char line[80];
+
+   fr = fopen ("elapsed.dta", "r");  /* open the file for reading */
+   /* elapsed.dta is the name of the file */
+   /* "rt" means open the file for reading text */
+
+   while(fgets(line, 80, fr) != NULL)
+   {
+	 sscanf (line, "%ld", &elapsed_seconds);
+	 printf ("%ld\n", elapsed_seconds);
+   }
+   fclose(fr);  /* close the file prior to exiting the routine */
    return 0;
+
 }
+
